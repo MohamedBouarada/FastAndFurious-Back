7b7fc3df6a2b6f9d03d6154d380efc7444235b13
MohamedBouarada <bouarada7.mb@gmail.com>
2022-04-26 02:55:54 +0100

    adding email text and backend is ready to be deployed

--- services/mail-service/mailService.js
+++ services/mail-service/mailService.js
@@ -18,7 +18,7 @@ class MailService{
                     from : "auto club",
                     to: participantMail,
                     subject : "inscription fast",
-                    text : "enregistrement done"
+                    text : "Cher participant, Félicitation pour votre inscription! La famille de Fast&Furious est ravie de vous avoir.C'est un pas en avant pour être parmis les compétiteurs dans nos challenges. Il ne vous reste que quelques tâches à accomplir: contacter le responsable du challenge pour payer les frais de l'inscription ET préparer vos robots et vos cerveaux pour une édition extraodinaire de Fast&Furious.Apportez vos compétences et votre bonne humeur, et préparer vous à avoir une ambiance à la fois compétitive et chaleureuse! Cordialement, Mariem SOULA, vice-présidente administrative."
                 }
 
             )
